fc2c9fcc0595eef1e94fbd65ac1476c46ec38098
dengjiayao <dnxbf321@gmail.com>
2016-02-17 19:36:33 +0800

    解决 after_render:css 巨慢的问题

--- scripts/after_render.js
+++ scripts/after_render.js
@@ -1,25 +1,21 @@
 'use strict';
 
 var autoprefixer = require('autoprefixer');
-var postcss = require('postcss-js');
+var postcss = require('postcss');
+var postcssJs = require('postcss-js');
 
 hexo.extend.filter.register('after_render:css', function(css, data) {
+  css = postcss.parse(css);
+  css = postcssJs.objectify(css);
 
-  if (data.path.indexOf('bower_components') === -1) {
+  var cssFlow = postcssJs.sync([
+    autoprefixer({
+      browsers: ['> 5%', 'last 3 versions', 'Firefox ESR', 'iOS >= 6', 'Android >= 4.0', 'ExplorerMobile >= 10']
+    })
+  ]);
 
-    var prefixer = postcss.sync([
-      autoprefixer({
-        browsers: ['> 5%', 'last 3 versions', 'Firefox ESR', 'iOS >= 6', 'Android >= 4.0', 'ExplorerMobile >= 10']
-      })
-    ]);
-    var result = prefixer(css);
+  var result = cssFlow(css);
 
-    return result.css;
-
-  } else {
-
-    return css;
-
-  }
+  return result.css;
 
 });
