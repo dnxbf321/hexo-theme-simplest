722704f6131500b58c18b58fbe66eb3567fd653e
dengjiayao <dnxbf321@gmail.com>
2016-02-18 14:48:07 +0800

    优化

--- scripts/sass_render.js
+++ scripts/sass_render.js
@@ -4,11 +4,10 @@ var sass = require('node-sass');
 var path = require('path');
 
 hexo.extend.renderer.register('scss', 'css', function(data, options) {
-
 	//将 scss 转换成 css
   var result = sass.renderSync({
     data: data.text,
-    includePaths: [path.join(__dirname, '../source/css'), path.join(__dirname, '../source/css/base')],
+    includePaths: [path.join(__dirname, '../source/css'), path.join(__dirname, '../source/css/base'), path.join(__dirname, '../source/bower_components/font-awesome/scss')],
     outputStyle: 'compressed',
     sourceMap: true
   });
