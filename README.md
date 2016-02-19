2cf75b351bd5e2f0df62ed8492481634a375a83e
dengjiayao <dnxbf321@gmail.com>
2016-02-19 13:28:04 +0800

    new feature

--- README.md
+++ README.md
@@ -1,13 +1,24 @@
 # hexo-theme-simplest
-hexo theme, written with swig &amp; scss
+hexo theme, written with swig &amp; sass
 
-##useage
+## usage
 ```
 git clone https://github.com/dnxbf321/hexo-theme-simplest.git themes/simplest
+cd themes/simplest
 npm install
 ```
 
-##日志
+## 日志
 
-###2015-12-06
+### 2016-2-19
+1. 图片延迟加载
+2. 首页滚动时，顶部显示当前位置的文章标题
+
+### 2016-2-18
+1. 压缩 js 代码
+2. 重命名 *.scss 文件，只输出必要的 css 文件，_ 前缀命名的 scss 文件不输出
+3. 使用 bower 管理第三方库
+4. 修复一些 bug
+
+### 2015-12-06
 更新依赖
---
+++ source/js/title-animate.js
@@ -0,0 +1,35 @@
+(function() {
+  var $titles = $('.post-content .title[data-role=articleTitle]');
+
+  var curText;
+  var $fix = $('#fixTitle');
+  function fixTitle(text) {
+    if (curText !== text) {
+      curText = text;
+      if (!$fix.length) {
+        $fix = $('<div id="fixTitle"><div class="center"><h1 class="title"></h1></div></div>');
+        $('body').append($fix);
+      }
+      $fix.show()
+        .find('.title').text(text);
+    }
+  }
+
+  function removeFixTitle() {
+    $fix.hide();
+  }
+
+  $(window).on('scroll', function() {
+    for (var i = $titles.length - 1; i >= 0; i--) {
+      var titleEl = $titles[i];
+      var rect = titleEl.getBoundingClientRect();
+      if (rect.top < 100) {
+        var text = $(titleEl).text();
+        fixTitle(text);
+        break;
+      } else if (i === 0) {
+        removeFixTitle();
+      }
+    }
+  });
+}());
